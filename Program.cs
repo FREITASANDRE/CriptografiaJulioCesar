c545043b873a4f61d2a96b6019aabbacf655d977
FREITASANDRE <36485479+FREITASANDRE@users.noreply.github.com>
2020-03-14 10:03:20 -0300

    Adicionando os arquivos

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConvertCriptJulioCesar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lModel = Message.GetMessage().Result;
+            if (lModel?.cifrado != null)
+            {
+                lModel.decifrado = EncriptDecript.DecriptMessage(lModel.cifrado.ToLower(), lModel.numero_casas);
+                lModel.resumo_criptografico = EncriptDecript.GenerateHashSha1(lModel.decifrado);
+                HandleFile.SaveFile("answer.json", lModel);
+                string lResponse = Message.PostMessage("answer.json").Result;
+                Console.WriteLine(lResponse);
+            }else
+                Console.WriteLine("Mensagem não foi recebida");
+            Console.ReadKey();
+        }      
+     
+    }
+}
